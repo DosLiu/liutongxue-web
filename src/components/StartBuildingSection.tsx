9cfec749e95bfd929214fc491c3504d74b2abf3b
DosLiu <86194291+DosLiu@users.noreply.github.com>
2026-04-06 22:17:29 +0800

    simplify contact card content and raise position

--- src/components/StartBuildingSection.tsx
+++ src/components/StartBuildingSection.tsx
@@ -1,4 +1,3 @@
-import { sitePaths } from '../site';
 import './StartBuildingSection.css';
 
 export default function StartBuildingSection() {
@@ -7,13 +6,8 @@ export default function StartBuildingSection() {
       <div className="start-building-container">
         <div className="start-building-card">
           <h2 id="start-building-title" className="start-building-title">
-            Start Exploring
+            与我链接
           </h2>
-          <p className="start-building-subtitle">Animations, Components, Backgrounds - One Click Away</p>
-
-          <a href={sitePaths.toolsPage} className="start-building-button">
-            Browse Components
-          </a>
         </div>
       </div>
     </section>
