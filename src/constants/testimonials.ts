748cbfe5d75474894940efee6e381bbc7d6d3169
Dosen <dosen@DosendeMac-mini.local>
2026-04-20 19:45:05 +0800

    chore: remove unused showcase and testimonial fields

--- src/constants/testimonials.ts
+++ src/constants/testimonials.ts
@@ -1,6 +1,5 @@
 export type TestimonialItem = {
   id: number;
-  avatar: string;
   text: string;
   handle: string;
 };
@@ -8,61 +7,51 @@ export type TestimonialItem = {
 export const TESTIMONIALS: TestimonialItem[] = [
   {
     id: 1,
-    avatar: 'https://pbs.twimg.com/profile_images/1794450494686932992/wqRqF4dt_400x400.jpg',
     text: '当技术的浪潮汹涌而来，我们需要的不只是游泳技巧，更需要学会驾驭帆船，乘风破浪',
     handle: '@发光hostel'
   },
   {
     id: 2,
-    avatar: 'https://pbs.twimg.com/profile_images/1918646280223608832/nqBF4zh__400x400.jpg',
     text: '你焦虑的不是AI更新太快，是你还没开始用它，就怕自己已经落后了',
     handle: '@小晨'
   },
   {
     id: 3,
-    avatar: 'https://pbs.twimg.com/profile_images/1593304942210478080/TUYae5z7_400x400.jpg',
     text: '你不用学完所有的AI工具，你只需要找到那一个，能帮你解决你当下问题的就够了',
     handle: '@微'
   },
   {
     id: 4,
-    avatar: 'https://pbs.twimg.com/profile_images/1722358890807861248/75S7CB3G_400x400.jpg',
     text: '技术更新的速度从来都快过人类学习的速度，但你要学的不是所有技术，是怎么用它帮你。',
     handle: '@搬砖哥'
   },
   {
     id: 5,
-    avatar: 'https://pbs.twimg.com/profile_images/1554006663853592576/Gxtolzbo_400x400.jpg',
     text: '你不用怕睡一觉就落后，怕的是你醒了，还在原地不动',
     handle: '@加班momo'
   },
   {
     id: 6,
-    avatar: 'https://pbs.twimg.com/profile_images/1880284612062056448/4Y2C8Xnv_400x400.jpg',
     text: '时代不会等你',
     handle: '@停滞的阿花'
   },
   {
     id: 7,
-    avatar: 'https://pbs.twimg.com/profile_images/1724192049002340352/-tood-4D_400x400.jpg',
     text: '现在还不晚，但是你再等一个月，就真的晚了',
     handle: '@跟不上阿芳'
   },
   {
     id: 8,
-    avatar: 'https://pbs.twimg.com/profile_images/1920165535351742464/CJU2uWMU_400x400.jpg',
     text: '在这个时代，普通人与专家的差距，只在于谁更擅长和我共事',
     handle: '@Google'
   },
   {
     id: 9,
-    avatar: 'https://pbs.twimg.com/profile_images/1756766826736893952/6Gvg6jha_400x400.jpg',
     text: 'AI具身是一代人一次的机会',
     handle: '@黄仁勋'
   },
   {
     id: 10,
-    avatar: 'https://pbs.twimg.com/profile_images/1957717329397141507/7ctDgOuc_400x400.jpg',
     text: 'AI不会直接淘汰人，而是拥抱AI的人，会把那些止步不前、抗拒AI的人淘汰',
     handle: '@周鸿祎'
   }
